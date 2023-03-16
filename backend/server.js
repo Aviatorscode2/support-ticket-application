088b8c52d5a43ca5f42a2fdd492b5e4dc8f1ea87
Aviatorscode2 <victoryakubu5@gmail.com>
2023-03-16 01:22:48 +0100

    Changes to the databse connection

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,14 @@ const  path = require('path');
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
+const mongoose = require('mongoose');
 const {errorHanddler} = require('./middleware/errorMiddleware');
-const connectDB = require('./config/db');
+// const connectDB = require('./config/db');
 const PORT = process.env.PORT || 5000;
 
 
 // Connect to database
-connectDB();
+// connectDB();
 // Create Express server
 const app = express();
 
@@ -18,6 +19,8 @@ app.get('/', (req, res) => {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
 // routes
 app.use('/api/users', require('./routes/userRoutes'));
